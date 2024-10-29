a8877672cc57e534fb30abf669c6602e4632e1f7
natallimir <natallimir82@mail.ru>
2024-10-29 18:02:37 +0500

    end chapter five

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -7,8 +7,12 @@ from ice_cream.models import IceCream
 def index(request):
     template_name = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        ).filter(is_on_main=True, is_published=True).order_by('title')[1:4]
+            'id', 'title', 'description', 'price'
+        ).filter(
+            is_on_main=True, 
+            is_published=True,
+            category__is_published=True
+            )
     context = {
         'ice_cream_list': ice_cream_list
     }
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -47,6 +47,11 @@ class IceCream(PublishedModel):
     is_on_main = models.BooleanField(default=False, verbose_name='На главную')
     title = models.CharField(max_length=256, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -66,6 +71,7 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = 'Мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
